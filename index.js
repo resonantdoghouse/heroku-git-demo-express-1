841314c1bb0fa1e8a502f5974fd43a7a0c1adb58
Jim <jim@advancebass.com>
2022-03-29 08:59:56 -0700

    log port

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Welcome to my Express App</h1>`);
+  console.log('PORT', process.env.PORT);
+  res.send(`<h1>Welcome to my Express App!</h1>`);
 });
 
 app.listen(PORT, () => {
-   console.log(`app running on port ${PORT}`)
+  console.log(`app running on port ${PORT}`);
 });
