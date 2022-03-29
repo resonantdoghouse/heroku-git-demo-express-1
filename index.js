49987071291a280ddacfbfe20cc575e6b615d2bf
Jim <jim@advancebass.com>
2022-03-29 09:11:20 -0700

    testing deploy from github

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   console.log('PORT', process.env.PORT);
-  res.send(`<h1>Welcome to my Express App!</h1>`);
+  res.send(`<h1>Welcome to my Express App!!! Deployed from GitHub</h1>`);
 });
 
 app.listen(PORT, () => {
